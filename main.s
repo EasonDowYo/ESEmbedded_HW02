270c2356e8b995872c847a6fdc284c2485847056
EasonDowYo <47811419+EasonDowYo@users.noreply.github.com>
2019-03-10 17:16:43 +0800

    Add files via upload
    Hw2 file

--- main.s
+++ main.s
@@ -6,15 +6,31 @@
 .global _start
 .type _start, %function
 _start:
-	nop
+	movs r0,#1
+    movs r1,#2
+    movs r2,#3
 
+    push {r0,r1,r2}
+
+    pop {r3,r4,r5}
+
+    movs r3,#0
+    movs r4,#0
+    movs r5,#0
+    
+    push {r2,r0,r1}
+
+    pop {r1,r2,r0}
+    
+    
+    
 	//
 	//branch w/o link
 	//
-	b	label01
+
+    b	label01
 
 label01:
-	nop
 
 	//
 	//branch w/ link
